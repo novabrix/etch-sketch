fd3170b151164aa42dd31a7119c26c8cb6b2aec5
novabrix <idriisAYusuf@gmail.com>
2025-04-09 19:54:38 -0500

    Interactivity work in progress

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ETCH-A-SKETCH
+version 1.8 - 4/9/2025
 version 1.5 - 4/6/2025
 version 1.0 - 4/5/2025
 
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log("JavaScript Connected!");
 const container = document.querySelector("div");
+const desc = document.querySelector("h3");
 
-const divMaker = function () {
+function defaultGrid() {
   for (let i = 1; i < 17; i++) {
     let holder = document.createElement("div");
     holder.className = `senior-div${i}`;
@@ -14,5 +15,33 @@ const divMaker = function () {
       holder.appendChild(smallHold);
     }
   }
-};
-divMaker();
+}
+defaultGrid();
+
+function newGrid() {
+  const divMaker = function () {
+    let prompter = prompt("Enter the square size you prefer! (Num x Num)");
+    let div = document.querySelector("div")
+    div.remove();
+    const container = document.createElement("div");
+    container.classList.add("container");
+    
+    if (3 < prompter < 24) {
+      for (let i = 1; i < 17; i++) {
+        let holder = document.createElement("div");
+        holder.className = `senior-div${i}`;
+        container.appendChild(holder);
+        console.log(`======= Senior-Div${i} created! =======`);
+        for (let i = 1; i < 17; i++) {
+          let smallHold = document.createElement("div");
+          smallHold.className = `junior-div${i}`;
+          console.log(`Junior-Div${i} created!`);
+          holder.appendChild(smallHold);
+        }
+      }
+    } else {
+      alert ("3 - 24 grid sizes allowed only!")
+    }
+  };
+  divMaker();
+}
