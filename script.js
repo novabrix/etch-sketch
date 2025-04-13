db3885d1da293a9500f1b54c948303565f38c7be
novabrix <idriisAYusuf@gmail.com>
2025-04-12 19:36:38 -0500

    Added colors and a few edits

--- script.js
+++ script.js
@@ -1,67 +1,71 @@
+"use strict"
 console.log("JavaScript Connected!");
 const container = document.querySelector("div");
 const desc = document.querySelector("h3");
+const body = document.querySelector("body");
+const seniorDiv = document.querySelector("div div");
+const juniorDiv = document.querySelector("div div div");
 
 function defaultGrid() {
   for (let i = 1; i < 16; i++) {
     let holder = document.createElement("div");
-    holder.className = `senior-div${i}`;
+    holder.className = `senior-div ${i}`;
     container.appendChild(holder);
-    console.log(`======= Senior-Div${i} created! =======`);
     for (let i = 1; i < 17; i++) {
       let smallHold = document.createElement("div");
-      smallHold.className = `junior-div${i}`;
+      smallHold.className = `junior-div ${i}`;
       smallHold.addEventListener(
         "mouseenter",
         (event) => {
-          event.target.style.backgroundColor = "red";
+          event.target.style.backgroundColor = "lightcoral";
           setTimeout(() => {
-            event.target.style.backgroundColor = "lightcoral";
-          }, 1500);
+            event.target.style.backgroundColor = "red";
+          }, 500);
         },
         false
       );
-      console.log(`Junior-Div${i} created!`);
       holder.appendChild(smallHold);
     }
   }
 }
 defaultGrid();
 
+function deleteGrid () {
+  console.log(seniorDiv, juniorDiv);
+  console.log(container)
+}
+
 function newGrid() {
   const divMaker = function () {
     let prompter = prompt("Enter the squares per side for the new grid! (Num X Num)");
-    /* let div = document.querySelector("div")
-    div.remove();
-    const container = document.createElement("div"); */
     container.classList.add("container");
 
-    if (3 < prompter < 24) {
+    if (prompter < 41 && prompter > 4) {
       for (let i = 1; i < prompter; i++) {
         let holder = document.createElement("div");
-        holder.className = `senior-div${i}`;
+        holder.className = `senior-div ${i}`;
         container.appendChild(holder);
-        console.log(`======= Senior-Div${i} created! =======`);
         for (let i = 1; i < prompter; i++) {
           let smallHold = document.createElement("div");
-          smallHold.className = `junior-div${i}`;
+          smallHold.className = `junior-div ${i}`;
           smallHold.addEventListener(
             "mouseenter",
             (event) => {
-              event.target.style.backgroundColor = "red";
+              event.target.style.backgroundColor = "lightcoral";
               setTimeout(() => {
-                event.target.style.backgroundColor = "lightcoral";
-              }, 3500);
+                event.target.style.backgroundColor = "red";
+              }, 500);
             },
             false
           );
-          console.log(`Junior-Div${i} created!`);
           holder.appendChild(smallHold);
         }
       }
     } else {
-      alert("3 - 24 grid sizes allowed only!");
+      alert("5 - 20 grid sizes allowed only!");
     }
   };
   divMaker();
 }
+let colors = ['red', 'orange', 'yellow', 'green', 'mediumblue', 'indigo',
+   'purple', 'gray', 'pink', 'brown', 'black', 'eraser' ]
